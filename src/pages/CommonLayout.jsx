b1a3b59ef2df569a4366f413de1d2c133dfebaec
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2023-12-10 21:22:48 +0700

    modified common layout

--- src/pages/CommonLayout.jsx
+++ src/pages/CommonLayout.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import { Loading } from "../components";
 
 const CommonLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <nav>
         <span className="text-4xl text-primary">Pyan Yaung</span>
       </nav>
-      <Outlet />
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
