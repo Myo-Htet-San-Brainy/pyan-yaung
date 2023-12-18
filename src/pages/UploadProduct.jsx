9212ae8a576731d3d47dcffe359f032f04e11359
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2023-12-18 22:41:52 +0700

    Upload Product Done But Need Refactoring

--- src/pages/UploadProduct.jsx
+++ src/pages/UploadProduct.jsx
@@ -1,12 +1,61 @@
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 import { FormCheckbox, FormInput, FormSelect, SubmitBtn } from "../components";
 import { categories } from "../assets/data";
+import { instance } from "../utils";
+import { toast } from "react-toastify";
 
 export async function action({ request }) {
-  const formEntries = await request.formData();
-  const data = Object.fromEntries(formEntries);
-  console.log(data);
-  return null;
+  try {
+    //get data
+    const formEntries = await request.formData();
+    const data = Object.fromEntries(formEntries);
+    const imageUrl = JSON.parse(localStorage.getItem("imageUrl"));
+    const isNegoInBoolean = data.isNego === "on" ? true : false;
+    const product = {
+      ...data,
+      image: imageUrl,
+      price: Number(data.price),
+      isNego: isNegoInBoolean,
+    };
+    //call api
+    const jwt = JSON.parse(localStorage.getItem("jwt"));
+    const res = await instance.post("/products", product, {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+    if (res.status === 201) {
+      toast.success("Your product is uploaded onto the site.");
+      return redirect("/myProducts");
+    }
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      "Upload was not successful. Please try again later.";
+    toast.error(errorMessage);
+    return null;
+  }
+}
+
+async function uploadImage(e) {
+  try {
+    const file = e.currentTarget.files[0];
+    const jwt = JSON.parse(localStorage.getItem("jwt"));
+    const res = await instance.post(
+      "/products/upload-product-image",
+      { image: file },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    const imageUrl = res.data.data;
+    localStorage.setItem("imageUrl", JSON.stringify(imageUrl));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const UploadProduct = () => {
@@ -19,23 +68,30 @@ const UploadProduct = () => {
         <h4 className="capitalize text-2xl font-bold text-center">
           Upload And Sell Your Product
         </h4>
-        <FormInput label="name" name="name" type={"text"} isRequired={true} />
+        <FormInput
+          label="name"
+          name="name"
+          type={"text"}
+          isRequired={true}
+          defaultValue={"chair"}
+        />
         <FormInput
           label={"price"}
           name={"price"}
           type={"number"}
           isRequired={true}
+          defaultValue={1000}
         />
         <FormCheckbox
           label={"Is Price Negotiable?"}
           name={"isNego"}
-          isRequired={true}
+          defaultValue={true}
         />
         <FormSelect
           label={"category"}
           name={"category"}
           list={categories}
-          isRequired={true}
+          defaultValue={"All"}
         />
         <label htmlFor="description">Description</label>
         <textarea
@@ -44,18 +100,21 @@ const UploadProduct = () => {
           cols="30"
           rows="5"
           required={true}
+          defaultValue={"Some description"}
         ></textarea>
         <FormInput
           label={"Phone Number"}
           name={"phNo"}
           type={"text"}
           isRequired={true}
+          defaultValue={"0994765425"}
         />
         <FormInput
           label={"Line Id"}
           name={"lineId"}
           type={"text"}
           isRequired={true}
+          defaultValue={"brainythemilkdrinker"}
         />
         <label className="label" htmlFor="image">
           <span className="label-text capitalize">Choose Product Image</span>
@@ -66,6 +125,7 @@ const UploadProduct = () => {
           name="image"
           accept="image/*"
           required={true}
+          onChange={uploadImage}
         />
         <div className="mt-8">
           <SubmitBtn text="Upload" />
