39bfa29676c4995f6c174fe4ee6806bc45010b17
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2024-10-16 02:23:55 +0700

    switched api url

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
-const baseurl = "https://pyan-yaung.as.r.appspot.com/api/v1";
+const baseurlAppEngine = "https://pyan-yaung.as.r.appspot.com/api/v1";
+const baseurlRender = "https://pyan-yaung.onrender.com/api/v1";
+const baseurlLocal = "http://localhost:5000/api/v1";
 
 export const instance = axios.create({
-  baseURL: baseurl,
+  baseURL: baseurlRender,
 });
